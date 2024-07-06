cdf2c82f3ec8a5eea7e8afdbe7c50d6228ec481b
Candy Tam <candytam9112001@gmail.com>
2024-07-06 12:41:39 +0800

    feat: edit profile page

---
+++ TikTok/frontend/components/unauthed-page/index.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from "react";
+
+import PageWithNavbar from "../page-with-navbar";
+import "./unauthed-page.scss";
+import { ComponentProps } from "../../../common/types";
+import { useAppDispatch } from "../../../common/store";
+import { authModalActions } from "../../../common/store/slices/auth-modal-slice";
+
+interface Props extends ComponentProps {
+	header: string;
+	options?: ReactNode;
+	icon: ReactNode;
+	description: string;
+}
+
+export default function UnauthedPage({
+	header,
+	icon,
+	description,
+	options
+}: Props) {
+	const dispatch = useAppDispatch();
+
+	function handleAuthModalOpen() {
+		dispatch(authModalActions.showModal());
+	}
+
+	return (
+		<PageWithNavbar containerClassName="unauthed-page">
+			<header>
+				<div />
+				<h4>{header}</h4>
+				<div>{options}</div>
+			</header>
+			<div className="content">
+				<div className="container">
+					{icon}
+					<p>{description}</p>
+					<button className="primary-button" onClick={handleAuthModalOpen}>
+						Log in
+					</button>
+				</div>
+			</div>
+		</PageWithNavbar>
+	);
+}
