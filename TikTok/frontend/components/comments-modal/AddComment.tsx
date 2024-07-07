f2c42d4b77517e0749096970053e8e6d9481b3fc
Candy Tam <candytam9112001@gmail.com>
2024-07-07 22:37:50 +0800

    fix: file directory

--- TikTok/frontend/components/comments-modal/AddComment.tsx
+++ TikTok/frontend/components/comments-modal/AddComment.tsx
@@ -3,7 +3,7 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 
 import "./comments-modal.scss";
-import Input from "../../../common/components/input-field";
+import Input from "../../../frontend/components/input-field";
 import constants from "../../../common/constants";
 import { useAppDispatch, useAppSelector } from "../../../common/store";
 import { errorNotification } from "../../helpers/error-notification";
