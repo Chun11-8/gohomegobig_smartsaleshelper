9cfb276f1f3c61cb712f6a0bbec3bceaa403dabc
Candy Tam <candytam9112001@gmail.com>
2024-07-07 22:47:32 +0800

    feat: auth modal common

---
+++ TikTok/common/components/auth-modal/index.tsx
@@ -0,0 +1,62 @@
+
+import { useState, useLayoutEffect } from "react";
+
+import "./auth-modal.scss";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import { useAppDispatch } from "../../store";
+import { joinClasses, modifyScrollbar } from "../../utils";
+import { authModalActions } from "../../store/slices/auth-modal-slice";
+import { ComponentProps } from "../../types";
+
+export interface FormProps {
+	setAuthType: React.Dispatch<React.SetStateAction<"login" | "signup">>;
+	handleModalClose: () => void;
+}
+
+interface Props extends ComponentProps {
+	isMobile?: boolean;
+}
+
+export default function AuthModal({ isMobile, className }: Props) {
+	const [authType, setAuthType] = useState<"login" | "signup">("login");
+	const dispatch = useAppDispatch();
+
+	useLayoutEffect(() => {
+		if (isMobile) return;
+		modifyScrollbar("hide");
+	}, [isMobile]);
+
+	function handleModalClose() {
+		if (!isMobile) modifyScrollbar("show");
+		dispatch(authModalActions.hideModal());
+	}
+
+	return (
+		<>
+			{!isMobile && (
+				<div className="backdrop auth-backdrop" onClick={handleModalClose} />
+			)}
+			<div
+				className={joinClasses(
+					"auth-modal-container",
+					isMobile && "mobile",
+					className
+				)}
+			>
+				{isMobile && <i className="fas fa-close" onClick={handleModalClose} />}
+				{authType === "login" ? (
+					<LogIn
+						setAuthType={setAuthType}
+						handleModalClose={handleModalClose}
+					/>
+				) : (
+					<SignUp
+						setAuthType={setAuthType}
+						handleModalClose={handleModalClose}
+					/>
+				)}
+			</div>
+		</>
+	);
+}
