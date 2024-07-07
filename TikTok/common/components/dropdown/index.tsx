46eed0292d2caf46b7350d795138e93e5a56d438
Candy Tam <candytam9112001@gmail.com>
2024-07-07 22:48:27 +0800

    feat: dropdown common

---
+++ TikTok/common/components/dropdown/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef, ReactNode } from "react";
+
+import classes from "./dropdown.module.scss";
+import { joinClasses, handleClickOutside } from "../../utils";
+import { ComponentProps } from "../../types";
+
+export interface DropdownProps extends ComponentProps {
+	children: ReactNode;
+	setShowDropdown?: React.Dispatch<React.SetStateAction<boolean>>;
+	trigger?: "click" | "hover";
+	onMouseOver?: () => void;
+	onMouseOut?: () => void;
+}
+
+export const DDAnimationTime = 100;
+
+export default function Dropdown({
+	className,
+	children,
+	setShowDropdown,
+	trigger = "click",
+	onMouseOver,
+	onMouseOut
+}: DropdownProps) {
+	const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (trigger !== "click") return;
+		const dropdown = dropdownRef.current!;
+		let timeOut: NodeJS.Timeout;
+
+		const eventRemover = handleClickOutside(dropdown, () => {
+			dropdown.classList.add(classes["hide"]);
+			timeOut = setTimeout(() => setShowDropdown!(false), DDAnimationTime);
+		});
+
+		return () => {
+			eventRemover();
+			clearTimeout(timeOut);
+		};
+	}, [setShowDropdown, trigger]);
+
+	return (
+		<div
+			className={joinClasses(classes["dropdown"], className)}
+			ref={dropdownRef}
+			onMouseOver={onMouseOver}
+			onMouseOut={onMouseOut}
+		>
+			{children}
+		</div>
+	);
+}
