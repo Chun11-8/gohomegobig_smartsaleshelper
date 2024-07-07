c5eb923e1d048f71fc81ebd2b480baa78e057ebd
Candy Tam <candytam9112001@gmail.com>
2024-07-07 23:05:30 +0800

    feat: alert store

---
+++ TikTok/common/store/index.ts
@@ -0,0 +1,24 @@
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./slices/auth-slice";
+import notificationReducer from "./slices/notification-slice";
+import authModalReducer from "./slices/auth-modal-slice";
+import PCReducer from "../../pc/store";
+import MobileReducer from "../../mobile/store";
+
+const store = configureStore({
+	reducer: {
+		auth: authReducer,
+		authModal: authModalReducer,
+		notification: notificationReducer,
+		pc: PCReducer,
+		mobile: MobileReducer
+	}
+});
+
+export default store;
+export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
+export const useAppSelector: TypedUseSelectorHook<
+	ReturnType<typeof store.getState>
+> = useSelector;
