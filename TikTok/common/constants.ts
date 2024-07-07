0616bfd398af9635025f4dab58357d2476ea6d21
Candy Tam <candytam9112001@gmail.com>
2024-07-07 23:07:52 +0800

    feat: constants common

---
+++ TikTok/common/constants.ts
@@ -0,0 +1,22 @@
+import { baseURL } from "./api";
+
+// eslint-disable-next-line
+export default {
+	usernameRegex: /^[a-zA-Z0-9_]+$/, // letters, numbers and underscore
+	usernameMinLen: 4,
+	usernameMaxLen: 15,
+	passwordMinLen: 6,
+	nameMaxLen: 30,
+	descriptionMaxLen: 300,
+	videoSizeLimit: 41943040, // 40MB
+	pfpRegex: /jpg|jpeg|png/i,
+	pfpSizeLimit: 2097152, // 2MB
+	musicMaxLen: 30,
+	captionMaxLen: 150,
+	tagsMaxLen: 200,
+	commentMaxLen: 300,
+	searchQueryMaxLen: 25,
+	pfpLink: baseURL + "/user/profilePhoto",
+	videoLink: baseURL + "/video/stream",
+	mobileWidth: 600 // any screen <= 600px will be considered mobile
+};
