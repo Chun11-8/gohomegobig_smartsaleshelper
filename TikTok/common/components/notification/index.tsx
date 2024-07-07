b70cba1d6c8577442878801cbd9c6c6986d37354
Candy Tam <candytam9112001@gmail.com>
2024-07-07 22:51:46 +0800

    feat: notification common

---
+++ TikTok/common/components/notification/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from "react";
+
+import classes from "./notification.module.scss";
+import { useAppDispatch } from "../../store";
+import { joinClasses } from "../../utils";
+import { notificationActions } from "../../store/slices/notification-slice";
+
+export interface NotificationProps {
+	type: "success" | "error" | "warning" | "info";
+	message: string;
+	isMobile?: boolean;
+}
+
+const notifDuration = 5000; // stays visible for 5s
+const notifAnimDuration = 1000; // hide animation plays for 1s
+
+export default function Notification({
+	type,
+	message,
+	isMobile
+}: NotificationProps) {
+	const dispatch = useAppDispatch();
+	const notifRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (!notifRef.current) return;
+		const classList = notifRef.current.classList;
+		setTimeout(() => classList.add(classes["reveal"]), 10);
+
+		const hideTimeout = setTimeout(() => {
+			classList.remove(classes["reveal"]);
+			classList.add(classes["hide"]);
+		}, notifDuration);
+		const removeTimeout = setTimeout(() => {
+			dispatch(notificationActions.hideNotification());
+		}, notifAnimDuration + notifDuration);
+
+		return () => {
+			clearTimeout(hideTimeout);
+			clearTimeout(removeTimeout);
+		};
+	}, [dispatch]);
+
+	return (
+		<div
+			ref={notifRef}
+			className={joinClasses(
+				classes["notification"],
+				isMobile && classes["mobile"],
+				classes[type]
+			)}
+		>
+			{message}
+		</div>
+	);
+}
